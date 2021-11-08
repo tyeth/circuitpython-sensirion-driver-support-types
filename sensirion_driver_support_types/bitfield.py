2afa41a2c9a428f5bd6f89e11e60306749e88bb5
Rolf Laich <rolf.laich@sensirion.com>
2021-11-08 09:02:19 +0100

    Add support for bitfields
    In many situations a integer contains many bits with specified
    semantics. In C this is represented by bitfields. Such bitfields
    are used in drivers as well. The added Classes allow to wrap
    an integer and to easily access the individual values that are
    encoded in this value.

---
+++ sensirion_driver_support_types/bitfield.py
@@ -0,0 +1,42 @@
+from collections import namedtuple
+
+BitField = namedtuple('BitField', ('offset', 'width'))
+
+
+class BitfieldContainer:
+
+    def __init__(self, int_value=0):
+        self._int_value = int_value
+
+    def __getattribute__(self, item):
+        attr = super(BitfieldContainer, self).__getattribute__(item)
+        if isinstance(attr, BitField):
+            return self._get_value(attr)
+        return attr
+
+    def __setattr__(self, key, value):
+        if hasattr(self, key):
+            attr = super(BitfieldContainer, self).__getattribute__(key)
+            if isinstance(attr, BitField):
+                self._set_value(attr, value)
+                return
+        super().__setattr__(key, value)
+
+    @staticmethod
+    def _get_mask(width: int) -> int:
+        return (1 << width) - 1
+
+    def _get_value(self, bitfield: BitField):
+        mask = self._get_mask(bitfield.width)
+        return (self._int_value >> bitfield.offset) & mask
+
+    def _set_value(self, bitfield: BitField, value: int):
+        mask = self._get_mask(bitfield.width)
+        self._int_value &= ~(mask << bitfield.offset)
+        self._int_value |= ((value & mask) << bitfield.offset)
+
+    @property
+    def value(self):
+        return self._int_value
+
+
