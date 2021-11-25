d0de8977e08afdf8e77e7cb5bdf9856e57245626
Rolf Laich <rolf.laich@sensirion.com>
2021-11-25 12:25:12 +0100

    Improve AccessBase implementation
    The implementation of the class AccessBase is improved. Namings are
    changed according to review comments. The unneeded code is removed and
    the AccessBase is now capable of accessing methods, classmethods and
    static methods.
    The class docstring explains the purpose of the class.

--- tests/test_base_class_access.py
+++ tests/test_base_class_access.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from sensirion_driver_support_types.BaseClassAccess import AccessBase
+from sensirion_driver_support_types.base_class_access import AccessBase
 
 
 class Abase:
@@ -10,6 +10,10 @@ class Abase:
 
     def get_name(self):
         return 'a_base'
+
+    @staticmethod
+    def static_fun():
+        return 'a_static_fun'
 
     @property
     def name(self):
@@ -24,6 +28,13 @@ class Bbase:
     def get_name(self):
         return 'b_base'
 
+    @classmethod
+    def class_fun(cls):
+        return 'b_class_fun'
+
+    @staticmethod
+    def static_fun():
+        return 'b_static_fun'
 
 class Single(Abase):
     base = AccessBase()
@@ -38,8 +49,23 @@ def test_single_base_access():
     single = Single()
     assert single.base.get_name() == 'a_base'
 
+def test_single_static_base_access():
+    single = Single()
+    assert single.base.static_fun() == 'a_static_fun'
+
 
 def test_multiple_base_access():
     cls = Multiple()
     assert cls.a_base.get_name() == 'a_base'
     assert cls.b_base.get_name() == 'b_base'
+
+
+def test_multiple_static_base_access():
+    cls = Multiple()
+    assert cls.a_base.static_fun() == 'a_static_fun'
+
+
+def test_multiple_class_base_access():
+    cls = Multiple()
+    assert cls.a_base.static_fun() == 'a_static_fun'
+    assert cls.b_base.class_fun() == 'b_class_fun'
